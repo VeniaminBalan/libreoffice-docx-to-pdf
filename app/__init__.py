daa087d19d34ff5555d3f14a383b17131b94cb8a
Balan Veniamin <veniamin.balan02@gmail.com>
2024-06-27 14:32:05 +0300

    removed cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .routes import convert_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     
     app.register_blueprint(convert_bp)
 
